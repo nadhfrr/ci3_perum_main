f8b0c0c6c6dc72a1c5443f9cf8c2fffa3f50cf39
Nadhief Fawaz Rezaka <ndhfwzproject@gmail.com>
2023-07-19 11:32:39 +0700

    bum
    sakalaka

--- application/views/index.php
+++ application/views/index.php
@@ -318,7 +318,6 @@
 
                 <div class="section-title">
                     <h2>Kontak</h2>
-                    <!-- <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p> -->
                 </div>
 
             </div>
